b40dba2c45e589c601eb15ec5dbf4017d1355541
czkirilo <caiquez.kirilo@hotmail.com>
2022-06-03 13:50:21 -0300

    Resolucao do erro
    O problema se encontrava no atributo tipo que tinhamos colocado como int mas na realidade deveria ser colocado como string

--- Chapter/Chapter/Models/Usuario.cs
+++ Chapter/Chapter/Models/Usuario.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; } 
         public string? Email { get; set; }
         public string Senha { get; set; }
-        public int Tipo { get; set; }
+        public string Tipo { get; set; }
     }
 }
 
